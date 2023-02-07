a3da7a476010004e7c05c2640d0d52df9cd77182
v-jacx <jvelasquez6798@gmail.com>
2023-02-07 07:43:23 -0500

    added logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'group_generator';
+  newName = '';
+  errorMessage = '';
+  numOfGroups : number | '' = '';
+  names : string[] = [];
+  groups : string [][] = [];
+
+  onInput =(memberName : string) => this.newName = memberName;
+  
+
+  addName =()=>{
+    if(!this.newName)this.errorMessage = 'Name can not be empty';
+    else{ 
+    this.names.push(this.newName);
+    this.newName = '';
+    this.errorMessage = ''
+  }
+   
+  }
+
+  onNumInput =(numOfGroups : string) => this.numOfGroups = Number(numOfGroups);
+
+  generateGroups = () =>{
+
+    const allNames = [...this.names];
+
+    if(!this.numOfGroups || this.numOfGroups >=0){
+      this.errorMessage = '# of groups must be a positive number'
+    }
+
+    while(allNames.length){
+      for(let i = 0; i < this.numOfGroups ; i++){
+        const randomIndex = Math.floor(Math.random() * allNames.length);
+
+        const person = allNames.splice(randomIndex, 1)[0];
+
+        if(!person)break;
+
+        if(this.groups[i]){
+          this.groups[i].push(person)
+        }else{
+          this.groups[i] = [person];
+        }
+      }
+      this.errorMessage = '';
+    }
+
+  }
+
 }
